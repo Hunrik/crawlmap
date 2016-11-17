fb3a84c7ac6e0c7b2de76fe590486f89a10dcf9b
Hunrik <henry.seres@gmail.com>
2016-11-17 09:44:56 +0100

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+const url = require('url');
+
+module.exports = class Sitemapper {
+  constructor(options) {
+    if (!options) throw new Error('Called without options');
+    if (!options.url) throw new Error('Missing parameter');
+    this.url = url.parse(options.url);
+  }
+};
---
+++ dist/test/index.js
@@ -0,0 +1,17 @@
+/* global describe, it */
+var chai = require('chai');
+var expect = chai.expect; // we are using the "expect" style of Chai
+var Mapper = require('../index.js');
+
+describe('Mapper', function () {
+  it('Should throw error if called without params', () => {
+    expect(() => new Mapper()).to.throw(Error);
+  });
+  it('Should throw error if called without url', () => {
+    expect(() => new Mapper({ something: 'Else' })).to.throw(Error);
+  });
+  it('Shoudld have property url', () => {
+    const mapper = new Mapper({ url: 'emag.hu' });
+    expect(mapper).to.have.property('url');
+  });
+});
