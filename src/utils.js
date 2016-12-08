7a0d339ffee106960353919c2d3141d0f10dbd35
Hunrik <henry.seres@gmail.com>
2016-12-08 09:12:27 +0100

    Babel fix

--- src/utils.js
+++ src/utils.js
@@ -13,14 +13,22 @@ exports.Request = (options) => {
       if (/.gz$/.test(options.url)) {
         const unzip = zlib.createUnzip()
         res.pipe(unzip)
+        let data = ''
+        unzip.on('data', function (chunk) {
+          data += chunk.toString('utf8')
+        })
+        unzip.on('end', function () {
+          return resolve(data)
+        })
+      } else {
+        let data = ''
+        res.on('data', function (chunk) {
+          data += chunk.toString('utf8')
+        })
+        res.on('end', function () {
+          return resolve(data)
+        })
       }
-      let data = ''
-      res.on('data', function (chunk) {
-        data += chunk.toString('utf8')
-      })
-      res.on('end', function () {
-        return resolve(data)
-      })
     })
   })
 }
